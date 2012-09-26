af74827f6ec1c694c37c74731c366e60f4864c2e
Humberto Pinheiro <humbhenri@gmail.com>
2012-09-26 13:05:17 -0300

    size constant

--- src/sudoclu/core.clj
+++ src/sudoclu/core.clj
@@ -2,7 +2,9 @@
   (:import [javax.swing JFrame JPanel JLabel JButton SwingUtilities]
            [java.awt GridLayout]))
 
-(def board (atom (vec (take 81 (cycle [0])))))
+(def *board-size* 81)
+
+(def board (atom (vec (take *board-size* (cycle [0])))))
 
 (defn read-sudoku
   "Read a string representing a sudoku board and populates the board.
@@ -17,11 +19,10 @@ Exemple of input
 .81...24.
 7..4.2..1"
   [input]
-  (let [a-char (to-array (filter #(not= \newline %) (.toCharArray input )))
+  (let [a-char (filter #(not= \newline %) (.toCharArray input ))
         char-to-int (fn [c] (let [i (- (int c) 48)]
                              (if (> i 0) i 0)))]
-    (dotimes [i 81]
-      (swap! board assoc i (char-to-int (aget a-char i))))))
+    (reset! board (vec (map char-to-int a-char)))))
 
 
 ;;; GUI
@@ -31,9 +32,9 @@ Exemple of input
 
 (def board-panel
   (let [panel (JPanel.)
-        squares (make-array JButton 81)]
+        squares (make-array JButton *board-size*)]
     (.setLayout panel (GridLayout. 9 9))
-    (dotimes [i 81]
+    (dotimes [i *board-size*]
       (let [square (make-square)]
         (.add panel square)
         (aset squares i square)))
@@ -42,7 +43,7 @@ Exemple of input
 (defn update-board-panel []
   (SwingUtilities/invokeLater
    (fn []
-     (dotimes [i 81]
+     (dotimes [i *board-size*]
        (let [square (aget (:squares board-panel) i)]
          (.setLabel square (str (nth @board i)))
          (.invalidate square)))
