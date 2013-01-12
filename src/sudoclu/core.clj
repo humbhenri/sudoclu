4840c7c4ce07438e8ea5ada7b81a76c74517ebd2
Humberto Pinheiro <humbhenri@gmail.com>
2013-01-11 23:58:42 -0200

    solve algorithm
    still a bug (returning nil)

--- src/sudoclu/core.clj
+++ src/sudoclu/core.clj
@@ -10,15 +10,24 @@
    (map (partial into []))
    (into [])))
 
-
 (defn to-str [board]
   (.replaceAll (with-out-str (pprint board)) "[\\[\\]]" ""))
 
-
 (defn next-empty [board]
-  (first (for [x (range 10) y (range 10) :when (= 0 (get-in board [x y]))]
+  (first (for [x (range 9) y (range 9) :when (= 0 (get-in board [x y]))]
            [x y])))
 
+(defn can-put? [board [row col] i]
+  (and (nil? (some #{i} (get board row)))                             ; test in row
+       (nil? (some #{i} (for [x (range 9)] (get-in board [x col])))) ; test in column
+       (nil? (some #{i} (for [x (take 3 (iterate inc (- row (mod row 3))))
+                              y (take 3 (iterate inc (- col (mod col 3))))]
+                          (get-in board [x y]))))))
 
 (defn solve [board]
-  )
+  (if-let [[x y] (next-empty board)]
+    (first (filter (comp nil? next-empty)
+                   (for [i (range 1 10)
+                         :when (can-put? board [x y] i)
+                         :let [board* (-> (assoc-in board [x y] i) (solve))]] board*)))
+    board))
